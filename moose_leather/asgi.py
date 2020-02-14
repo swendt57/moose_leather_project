16b4edd026df854aaada17c313cd71a7581dfa08
swendt57 <Lthr4ever>
2020-02-14 08:18:54 -0800

    initial commit - django framework and basic file structure in place

---
+++ moose_leather/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for moose_leather project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'moose_leather.settings')
+
+application = get_asgi_application()
