bc04f34cf989fa8290ff7e0dd8b1f36f533ab370
swendt57 <Lthr4ever>
2020-02-16 15:53:00 -0800

    added Heroku url to allowed hosts

--- moose_leather/settings.py
+++ moose_leather/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ev=(2fp44xekrc27ay@dv&p^(gp=x6p915*sqepw-g3y7b3f*&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'moose-leather.herokuapp.com']
 
 
 # Application definition
